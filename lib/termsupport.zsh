18f3e7695dc51c4694f95d700592f6e3441d562b
Gabriel Ebner <gebner@2b7e.org>
2013-03-29 15:24:00 +0100

    Don't expand command substitutions in screen titles.

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -6,12 +6,14 @@ function title {
   if [[ "$DISABLE_AUTO_TITLE" == "true" ]] || [[ "$EMACS" == *term* ]]; then
     return
   fi
+  setopt +o promptsubst
   if [[ "$TERM" == screen* ]]; then
     print -Pn "\ek$2:q\e\\" #set screen hardstatus, usually truncated at 20 chars
   elif [[ "$TERM" == xterm* ]] || [[ $TERM == rxvt* ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
     print -Pn "\e]2;$2:q\a" #set window name
     print -Pn "\e]1;$1:q\a" #set icon (=tab) name (will override window name on broken terminal)
   fi
+  setopt -o promptsubst
 }
 
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
